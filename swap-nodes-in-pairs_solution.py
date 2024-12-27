06dcfc38bbf0b17398de26e8011f9bda86a91ec6
qiliang.huang <goosehuangmatt@gmail.com>
2024-12-27 08:51:51 +0800

    Remove implementations of swapping nodes in pairs from the repository

--- swap-nodes-in-pairs_solution.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def swapPairs(self, head: ListNode) -> ListNode:
-        if not head or not head.next:
-            return head
-        newHead = head.next
-        head.next = self.swapPairs(newHead.next)
-        newHead.next = head
-        return newHead
